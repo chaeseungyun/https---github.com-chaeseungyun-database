083032fd737722352d8a7bdb2e3285cd75642156
SangG <82752746+Choi-sanggyu@users.noreply.github.com>
2023-06-10 20:11:03 +0900

    Add files via upload

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -4,16 +4,13 @@ import { Link } from "react-router-dom";
 export default function LoginButton() {
   return (
     <div className="button-container">
-      <span className="quote">여러분의 보안을 지켜드립니다.</span>
+      <span className="quote">붕어빵 마을에 어서오세요</span>
       <Link to="/login">
-        <button className="login-button">마을 들어가기</button>
+        <button className="login-button">마을 들어가기(로그인)</button>
       </Link>
       <div className="find-set">
         <Link to='/find-id-password'>
-          <div className="id-pw">
-            <span className="id">아이디</span>
-            <span className="pw">비밀번호 찾기</span>
-          </div>
+            <span className="id-pw">아이디 / 비밀번호 찾기</span>
         </Link>
         <Link to='/join'>
           <div className="join">
